6730dba8b08705c02e9706ffe7c704e9ccb7dc46
MaryamAbdullateef <mabdullatteef80@gmail.com>
2026-05-11 13:03:53 +0100

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { useEffect } from "react";
 
-// Layout Components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-// Existing Pages
 import Home from "./pages/Home";
 import Women from "./pages/Women";
 import Men from "./pages/Men";
@@ -16,14 +14,13 @@ import ContactPage from "./pages/ContactPage";
 import Order from "./pages/Order";
 import Collections from "./pages/Collections";
 
-// New Feature Pages (The ones we just created)
 import TrackOrder from "./pages/TrackOrder";
 import ReturnsPolicy from "./pages/ReturnsPolicy";
 
 // Feature Pages
-import Account from "./pages/account";
-import Search from "./pages/search";
-import AddToCart from "./pages/add-to-cart";
+import Account from "./pages/Account";
+import Search from "./pages/Search";
+import AddToCart from "./pages/AddToCart";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -36,7 +33,6 @@ function ScrollToTop() {
 export default function App() {
   const location = useLocation();
 
-  // Premium UI: No Navbar/Footer on login page
   const isAccountPage = location.pathname === "/account";
 
   return (
@@ -47,24 +43,20 @@ export default function App() {
 
       <main className="grow">
         <Routes>
-          {/* --- BASE ROUTES --- */}
           <Route path="/" element={<Home />} />
           <Route path="/men" element={<Men />} />
           <Route path="/kids" element={<Kids />} />
           <Route path="/women" element={<Women />} />
 
-          {/* --- NEW SUPPORT ROUTES --- */}
           <Route path="/track-order" element={<TrackOrder />} />
           <Route path="/returns-policy" element={<ReturnsPolicy />} />
 
-          {/* --- OTHER ROUTES --- */}
           <Route path="/collections" element={<Collections />} />
           <Route path="/new" element={<NewArrival />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/account" element={<Account />} />
           <Route path="/search" element={<Search />} />
 
-          {/* --- PROTECTED ROUTES --- */}
           <Route
             path="/order"
             element={
@@ -82,7 +74,6 @@ export default function App() {
             }
           />
 
-          {/* --- CATCH-ALL REDIRECT --- */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
